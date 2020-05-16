eb467649e948e0570ef1669fa878b9663fc97918
EBEREGIT <samsoneberenjoku@gmail.com>
2020-05-16 10:11:19 +0100

    added delete and edit button. Deleted the react logo and favicons

--- src/components/chat/AllChats.js
+++ src/components/chat/AllChats.js
@@ -6,22 +6,13 @@ let items = [];
 for (let index = 0; index < 10; index++) {
   items.push(
     <Row className="all-chats">
-    <Col lg={4}>
-    <Image
-      src="https://pngimage.net/wp-content/uploads/2019/05/dummy-man-png-2.png"
-      width="100%"
-      height="50%"
-      alt="profile AVI"
-      roundedCircle
-    />
-  </Col>
-
-  <Col lg={8}>
+  <Col lg={12}>
     <Card style={{ width: "100%" }}>
+      <Card.Title>Poster Name</Card.Title>
       <Card.Img
         variant="top"
         src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        height="100%"
+        height="auto"
         width="100%"
       />
       <Card.Body>
@@ -29,7 +20,8 @@ for (let index = 0; index < 10; index++) {
         <Card.Text>
           Article Body
         </Card.Text>
-        <Button variant="primary">View More</Button>
+        <Button variant="primary">Edit</Button>
+        <Button variant="danger">Delete</Button>
       </Card.Body>
     </Card>
   </Col>
