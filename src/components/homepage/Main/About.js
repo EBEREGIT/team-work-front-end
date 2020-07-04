75189b0230c0d79074580ddcc3c1f852f3711e29
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-04 10:41:04 +0100

    ft(About Us): Added video and call to action

--- src/components/homepage/Main/About.js
+++ src/components/homepage/Main/About.js
@@ -1,15 +1,45 @@
-import React, { Fragment } from 'react'
-import { Row } from "react-bootstrap";
+import React, { Fragment } from "react";
+import { Row, Col } from "react-bootstrap";
 
 export default function About() {
-    return (
-        <Fragment>
-            <h4 className="headings">About</h4>
-            <Row>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non sapien ut lorem tristique consectetur. Curabitur eu sapien at purus venenatis porttitor. Nam blandit lobortis tellus, sed tincidunt ante posuere quis. Cras ut arcu nisl. Suspendisse ipsum turpis, facilisis quis porttitor vitae, gravida a nisl. Vestibulum aliquet ex at turpis lacinia porttitor. Sed quis lorem ac sapien congue vulputate. Phasellus dictum, sem sagittis sagittis rhoncus, felis nisl consectetur orci, ut porttitor dui neque id neque. Donec lobortis massa at aliquam semper. Sed dolor sem, facilisis ut aliquet vel, bibendum id nibh.
-                </p>
-            </Row>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <h4 className="headings">About</h4>
+      <Row>
+        {/* video pitch */}
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <iframe
+            width="100%"
+            height="100%"
+            title="Pitching Team Work"
+            src="https://www.youtube.com/embed/Q4vtfku9qyY"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Col>
+
+        {/* text */}
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non
+            sapien ut lorem tristique consectetur. Curabitur eu sapien at purus
+            venenatis porttitor. Nam blandit lobortis tellus, sed tincidunt ante
+            posuere quis. Cras ut arcu nisl. Suspendisse ipsum turpis, facilisis
+            quis porttitor vitae, gravida a nisl. Vestibulum aliquet ex at
+            turpis lacinia porttitor. Sed quis lorem ac sapien congue vulputate.
+            Phasellus dictum, sem sagittis sagittis rhoncus, felis nisl
+            consectetur orci, ut porttitor dui neque id neque. Donec lobortis
+            massa at aliquam semper. Sed dolor sem, facilisis ut aliquet vel,
+            bibendum id nibh.
+          </p>
+
+          {/* call to action button */}
+          <a href="/signup" className="btn btn-success btn-block">
+            Join Team Work
+          </a>
+        </Col>
+      </Row>
+    </Fragment>
+  );
 }
